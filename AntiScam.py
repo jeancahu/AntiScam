00494133eb20e2874cc339539d3cd74041d37a4c
Jeancahu <jeancahu@gmail.com>
2021-11-23 16:05:17 -0600

    add: default values

--- AntiScam.py
+++ AntiScam.py
@@ -6,13 +6,28 @@ last_message_content = ''
 spam_counter = 0
 
 
-async def AntiScam(message, bot, whitelist, muted_role, verified_role, logs_channel):
-    global message_content, last_message, last_message_content, spam_counter
-    message_content = f'{message.author.id}: {message.content}'
-    message_content = message_content.replace("'", "`")
+async def AntiScam(
+        message,
+        bot,
+        white_list = [],
+        muted_role = "Muted",
+        verified_role = "Verified",
+        logs_channel = 0):
+
+    global message_content, \
+        last_message, \
+        last_message_content, \
+        spam_counter
+
+    message_content = "{}: {}" \
+        .format(message.author.id, message.content) \
+        .replace("'", "`")
 
     # AntiScam-System
-    if message_content == last_message_content and message.content != '' and message.author.id not in whitelist:
+    if message_content == last_message_content and \
+       message.content != '' and \
+       message.author.id not in whitelist:
+
         spam_counter += 1
         await message.delete()
     else:
@@ -31,5 +46,8 @@ async def AntiScam(message, bot, whitelist, muted_role, verified_role, logs_chan
         await last_message.delete()
         await message.author.add_roles(muted)
         await message.author.remove_roles(verified)
-        await logs_channel.send(f'USUARIO MUTEADO: {message_content}')
-    await bot.process_commands(message)
+
+        if Type(logs_channel) == int:
+            logs_channel = bot.get_channel (logs_channel)
+
+        await logs_channel.send("USUARIO MUTEADO: {}".format(message_content))
